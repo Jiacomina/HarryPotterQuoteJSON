dc943ea8c9a7fbfe08a0427a7f1e26ce1ff72459
Jacquie Soon <jacqueline.gakmay.soon@gmail.com>
2017-06-27 12:21:19 +0800

    HTML, CSS, JS

---
+++ js/index.js
@@ -0,0 +1,37 @@
+var jsonFile = [];
+var id = -1;
+$.getJSON("https://api.myjson.com/bins/18yq43.json?", function(json) {
+	jsonFile = json;
+});
+function getFact() {
+	var nFacts = jsonFile.length;
+	var randomNum = 0;
+	randomNum = Math.floor(Math.random() * nFacts);
+	$(".quote").html(JSON.stringify(jsonFile[randomNum].Quote));
+	id = randomNum;
+}
+
+$(document).ready(function() {
+	$("#quote-button").on("click", function(event) {
+		getFact();
+	});
+	
+	$("#like").on("click", function(event){
+		if(id != -1){
+			console.log("liked: " + id);
+			json(id).likes++;
+			id = -1;
+		}
+	});
+	$("#dislike").on("click", function(event){
+		if(id != -1){
+			console.log("disliked: " + id);
+			json(id).dislikes++;
+			id = -1;
+		}
+	});
+});
+
+/*	
+	
+	*/
